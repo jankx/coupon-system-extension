3249cf153aae6ea59ba88c4982858149020113d5
Puleeno Nguyen <puleeno@gmail.com>
2026-08-02 22:32:37 +0700

    chore: initialize Composer autoloader for coupon system extension

---
+++ libs/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jankx/coupon-system',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '77bc475f136c0ada1c36dddfd8f67e2d010c7143',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jankx/coupon-system' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '77bc475f136c0ada1c36dddfd8f67e2d010c7143',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
