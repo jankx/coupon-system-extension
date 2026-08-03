b2b9b1d5e186cfbdee196cc0fd2db48d9ac8f9e9
Puleeno Nguyen <puleeno@gmail.com>
2026-08-03 19:42:49 +0700

    Update coupon tab label to English

--- CouponSystemExtension.php
+++ CouponSystemExtension.php
@@ -63,7 +63,7 @@ class CouponSystemExtension extends AbstractExtension
     public function registerAccountSubPage(): void
     {
         \Jankx\Extensions\MyAccount\MyAccountExtension::registerSubPage('coupons', [
-            'label' => 'Mã ưu đãi',
+            'label' => 'Coupons',
             'icon' => '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 12V8H6a2 2 0 0 1-2-2c0-1.1.9-2 2-2h12v4"/><path d="M4 6v12c0 1.1.9 2 2 2h14v-4"/><path d="M18 12a2 2 0 0 0 0 4h4v-4z"/></svg>',
             'priority' => 20,
             'extension' => 'coupon-system',
