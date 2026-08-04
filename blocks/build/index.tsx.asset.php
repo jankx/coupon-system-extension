a527d86f8f8d908065631a044350cc102fc9571f
Puleeno Nguyen <puleeno@gmail.com>
2026-08-04 21:04:14 +0700

    feat: update coupon block parent, remove spacing support, and add responsive visibility attributes

--- blocks/build/index.tsx.asset.php
+++ blocks/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'e53b207064acb8f2271a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '6d53960853a9dc3e3efb');
