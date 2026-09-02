0594a7ea587fd0b0e95373b8c6f13ade9a858448
Puleeno Nguyen <puleeno@gmail.com>
2026-09-03 04:47:05 +0700

    fix: update asset version for coupon block and improve code structure

--- blocks/build/index.tsx.asset.php
+++ blocks/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '6d53960853a9dc3e3efb');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '4bfa10aa9b6f8cd7960c');
