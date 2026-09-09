37b6c34a29b8bffd4ed0d777bbf7a6ef61918a91
Puleeno Nguyen <puleeno@gmail.com>
2026-09-10 03:42:39 +0700

    feat: enable public access and queryability for the coupon post type

--- src/PostTypes/CouponPostType.php
+++ src/PostTypes/CouponPostType.php
@@ -66,12 +66,12 @@ class CouponPostType
 
         $args = [
             'labels' => $labels,
-            'public' => false,
-            'publicly_queryable' => false,
+            'public' => true,
+            'publicly_queryable' => true,
             'show_ui' => true,
             'show_in_menu' => true,
             'show_in_rest' => true,
-            'query_var' => false,
+            'query_var' => 'coupon',
             'rewrite' => array(
                 'slug' => 'coupon'
             ),
