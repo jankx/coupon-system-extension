a7d95245d76b06f43c4f62dbaadb0bcaa749ea10
Puleeno Nguyen <puleeno@gmail.com>
2026-09-25 18:19:47 +0700

    refactor: remove unused CSS and map files for available coupons block

--- blocks/available-coupons/build/index.tsx.asset.php
+++ blocks/available-coupons/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c0d09ecae2d9f2703a3e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '23dece9a158a98af621d');
